2e3d578bd0f3f1f967a442d577cb9df17f467262
afokepeniel <afokekowho@gmail.com>
2024-10-25 00:45:15 -0400

    day2a code modification

--- BMI-Calulator-Day2a.py
+++ BMI-Calulator-Day2a.py
@@ -2,7 +2,7 @@
 Create a program that calculates a person's Body Mass Index (BMI).
 It asks for the user's weight and height repsectively.
 Covert the height to float datatype and Covert the weight to int data type respectively.
-But the final answer for the person's BMI should be in integer.
+But the final answer for the person's BMI should be rounded up to 2 decimal places.
 
 """ 
 
