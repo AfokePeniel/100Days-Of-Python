fb18ea0e6d3fe9ef7629f09fa0f275e77c1e951a
afokepeniel <afokekowho@gmail.com>
2024-10-25 00:02:37 -0400

    code modification Day1

--- GreetingProgram-Day1.py
+++ GreetingProgram-Day1.py
@@ -4,10 +4,10 @@ The program asks for the city the user grew up in and also the user's pet name i
 Next it combines both input together to form a Band name.
 """
 
-#Greeting Program
+#Band Generator Program
 
-greeting = "Welcome"
-print(greeting)
+greetings = "Welcome to Band Name Generator"
+print(greetings)
 
 city_name = input("Enter the name of city you grew up in \n")
 pet_name = input("Enter the name of your pet \n")
